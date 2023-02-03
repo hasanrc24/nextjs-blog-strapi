def9ced2e996089f105e7851a89554a430f98c96
hasanrc24 <hasanrc24@gmail.com>
2023-02-03 17:32:14 +0600

    redeploy

--- config/env/production/database.js
+++ config/env/production/database.js
@@ -1,33 +1,13 @@
-// module.exports = ({ env }) => ({
-//   connection: {
-//     client: "postgres",
-//     connection: {
-//       host: env("PGHOST", "127.0.0.1"),
-//       port: env.int("PGPORT", 5931),
-//       database: env("PGDATABASE", "railway"),
-//       user: env("PGUSER", "postgres"),
-//       password: env("PGPASSWORD", "password"),
-//       ssl: env.bool(true),
-//     },
-//   },
-// });
-
-const { parse } = require("pg-connection-string");
-
-module.exports = ({ env }) => {
-  const { host, port, database, user, password } = parse(env("DATABASE_URL"));
-
-  return {
+module.exports = ({ env }) => ({
+  connection: {
+    client: "postgres",
     connection: {
-      client: "postgres",
-      connection: {
-        host,
-        port,
-        database,
-        user,
-        password,
-      },
-      debug: false,
+      host: env("PGHOST", "127.0.0.1"),
+      port: env.int("PGPORT", 5931),
+      database: env("PGDATABASE", "railway"),
+      user: env("PGUSER", "postgres"),
+      password: env("PGPASSWORD", "password"),
+      ssl: env.bool(true),
     },
-  };
-};
+  },
+});
