5871fc3fd8bde01e72103681a290823c1ec07404
hasanrc24 <hasanrc24@gmail.com>
2023-03-02 23:49:57 +0600

    as

--- src/api/article/routes/article.js
+++ src/api/article/routes/article.js
@@ -1,9 +1,5 @@
-'use strict';
+"use strict";
 
-/**
- * article router
- */
+const { createCoreRouter } = require("@strapi/strapi").factories;
 
-const { createCoreRouter } = require('@strapi/strapi').factories;
-
-module.exports = createCoreRouter('api::article.article');
+module.exports = createCoreRouter("api::article.article");
